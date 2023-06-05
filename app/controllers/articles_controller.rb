fc05c17973e98c55c8bf2ec847b226721aa951c8
Ryu <azatose8684@gmail.com>
2023-06-05 12:06:05 +0900

    login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,38 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update]
+  def edit
+  end
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def new
+    @article = Article.new
+  end
+  def show
+  end
   def index
     @articles = Article.all
   end
-  def show
-    
+  
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_url(@article), notice: 'Article was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
